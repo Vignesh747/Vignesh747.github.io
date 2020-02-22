db4f943f7b859697059f0cb2bff0059ef114e59f
Vignesh747 <vikiwiki747@gmail.com>
2020-02-22 15:09:39 +0800

    Yo whats up

---
+++ js/index.js
@@ -0,0 +1,25 @@
+
+// Do not amend these!
+function run() {
+    var input = document.getElementById("input").value.split(" ");
+    if (input.length >= 3) {
+      main(parseInt(input[0]), parseInt(input[1]), parseInt(input[2]));
+    } else {
+      console.log("Please enter 3 numbers.");
+    }
+  }
+  
+  function main(a, b, c) {
+    // Use a, b and c to help you!
+    // TODO: Your code below.
+
+  }
+
+  var target = document.getElementById('target');
+  target.innerHTML = 'I am cool';
+  
+  function hello(e) {
+      console.log(e);
+  }
+  window.addEventListener("resize", hello);
+  
